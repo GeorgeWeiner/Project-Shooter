0af47f6cd7b020e18f99a9cd8500f5308bbb24ec
Enzo Marchetti <enzo.marchetti97@gmail.com>
2022-03-15 21:00:32 +0100

    Lol added most generic event system ever

--- Assets/ScriptableObjects/ScriptableEvents/ScriptableEventScripts/AudioEvent.cs
+++ Assets/ScriptableObjects/ScriptableEvents/ScriptableEventScripts/AudioEvent.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "ScriptableEvent/AudioEvent",fileName ="ScriptableEvent",order = 0)]
 public class AudioEvent : ScriptableEvent
 {
-    [SerializeField] AudioClip audioToPlay;
-    public override void EventFunction()
+    [SerializeField] List<AudioClip> audioClipsToPlay;
+    protected override void EventFunction<Transform>( Transform soundPos)
     {
-      
+        for (int i = 0; i < audioClipsToPlay.Count; i++)
+        {
+            var tempObj = new GameObject();
+            tempObj.transform.position = soundPos.transform.position;
+            var tempAudioSource = tempObj.AddComponent<AudioSource>();
+            tempAudioSource.PlayOneShot(audioClipsToPlay[i]);
+            Destroy(tempObj, audioClipsToPlay[i].length);
+            Debug.Log("Hello");
+        }  
     }
-
+    //public override void StartEvent<T>(T parameter)
+    //{
+    //    base.StartEvent(parameter);
+    //}
 }
--- Assets/ScriptableObjects/ScriptableEvents/ScriptableEventScripts/ScriptableEvent.cs
+++ Assets/ScriptableObjects/ScriptableEvents/ScriptableEventScripts/ScriptableEvent.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using BaseScripts;
 using UnityEngine;
 
-public abstract class ScriptableEvent : MonoBehaviour
+public enum TypeOfThisEvent
 {
-    [SerializeField] Timer eventTimer;
-    public abstract void EventFunction();
+    audioEvent,
+    enemyEvent
+}
+public abstract  class ScriptableEvent : ScriptableObject
+{
+    [SerializeField] protected TypeOfThisEvent typeOfThisEvent;
+    public TypeOfThisEvent TypeOfThisEvent { get { return typeOfThisEvent; } }
+    [SerializeField] protected Timer eventTimer;
+    [SerializeField] float eventDelay;
+    protected abstract void EventFunction<T>(T parameter) where T : Transform;
+    public virtual void StartEvent<T>(T parameter) where T : Transform
+    {
+        EventFunction<T>(parameter);
+    }
+   
 }
